e23c221cb951a4d1f3018337aa1e8fac74508f02
Lan Phan <lanc_phan@hotmail.com>
2021-02-21 20:50:57 -0500

    update the store schema to have the location of the store

--- models/Store.js
+++ models/Store.js
@@ -6,17 +6,36 @@ const storeSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: 'Please enter a store name!'
+    required: 'Please enter a store name!',
   },
   slug: String,
   description: {
     type: String,
-    trim: true
+    trim: true,
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [{ type: Number, required: 'You must supply coordinates!' }],
+    address: {
+      type: 'String',
+      required: 'You must supply an address!',
+    },
+  },
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', function (next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
